f7c821bb5e0aa535f20d063c7790662b0e27db7a
Teodor Lysvret van Ginhoven <teodorvanginhoven@gmail.com>
2017-04-03 17:11:09 +0200

    Added some comments

--- Source-Code/GlobalVariables.cpp
+++ Source-Code/GlobalVariables.cpp
@@ -3,12 +3,12 @@
 #include <Windows.h>
 
 namespace GV {
-	std::fstream settings;
-	POINT firstPos;
-	POINT secondPos;
-	///Open/Creates the path.txt file, that includes the path of VLC player
-	std::fstream path;
-	int msg;
-	std::string font;
-	bool hideWindow;
+	std::fstream settings;   //Settings file
+	POINT firstPos;   //Mouse position over the first VLC window/any player
+	POINT secondPos;   //Mouse position over the second window
+	///Could probably have the paths in the settings file, but not now
+	std::fstream path;   //the path.txt file
+	int msg;   //The message box variable
+	std::string font;   //the font path
+	bool hideWindow;   //If you want to hide program when play/pauseing the players
 }
