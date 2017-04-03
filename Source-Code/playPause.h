4843adee028b76f3f4dc47d9a5185b0a43e2c3a6
Teodor Lysvret van Ginhoven <teodorvanginhoven@gmail.com>
2017-04-03 15:54:12 +0200

    Update playPause.h

--- Source-Code/playPause.h
+++ Source-Code/playPause.h
@@ -18,33 +18,27 @@ private:
 	///Second VLC player information 
 	STARTUPINFO si2;
 	PROCESS_INFORMATION pi2;
-	///Open/Creates the path.txt file, that includes the path of VLC player
-	std::fstream path;
+
 	///Enables the CreateProcess to fire if the path was found
 	bool canopen = false;
 	LPCWSTR vlc;
 
 	std::string h;
 	std::wstring j;
-	int msg;
+
 	INPUT key = { 0 };
 	POINT prePos;
-	POINT firstPos;
-	POINT secondPos;
 	DWORD pid;
-	std::fstream settings;
 	int pidw;
 	int pi2dw;
 	const short sleepTime = 200;
 public:
-	std::string font;
-	bool hideWindow;
+
 	playPause();
 	~playPause();
 	void setCursorVar(std::string b);
 	void Play(sf::RenderWindow &window);
 	void leftclick();
-	void SetupStuff();
 	void startVLC();
 
 };
