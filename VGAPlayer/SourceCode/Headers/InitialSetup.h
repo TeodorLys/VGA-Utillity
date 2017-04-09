6a4dfc0ceed335f6b0d3f029776f3b0f3419ee13
Teodor Lysvret van Ginhoven <teodorvanginhoven@gmail.com>
2017-04-10 01:30:52 +0200

    Update InitialSetup.h

--- VGAPlayer/SourceCode/Headers/InitialSetup.h
+++ VGAPlayer/SourceCode/Headers/InitialSetup.h
@@ -1,11 +1,19 @@
 #pragma once
+#include <string>
+#include <SFML\Graphics.hpp>
 class InitialSetup
 {
+private:
+
 public:
 	InitialSetup();
 	~InitialSetup();
+	std::string ffs;
+	std::string mov;
 	void movieWindow();
 	void UI();
 	void files();
+	void openFile(int n);
+	void LoadMovie(int n);
 };
 
