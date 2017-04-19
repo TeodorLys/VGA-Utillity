1da3de06622cadc7991024f25518bf404ffd1694
Teodor Lysvret van Ginhoven <teodorvanginhoven@gmail.com>
2017-04-19 02:19:07 +0200

    Add files via upload

--- VGAPlayer/SourceCode/Headers/EventHeaders/mouseEvent.h
+++ VGAPlayer/SourceCode/Headers/EventHeaders/mouseEvent.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Events.h"
+#include "ContextMenu.h"
 #include <chrono>
 
 class mouseEvent : public Events {
@@ -12,6 +13,9 @@ public:
 	void menuButtons();
 	void checkUI(std::chrono::high_resolution_clock::time_point &tStart, std::chrono::high_resolution_clock::time_point &tEnd);
 	void showHideUI();
-
+	void showContextMenu(ContextMenu &cM);
+	void showContextMenuSelector(ContextMenu &cM);
+	void contextMenuSetup();
+	void contextMenuHandler();
 };
 
